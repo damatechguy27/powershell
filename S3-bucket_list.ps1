6a9ecb5674c51785ea10bb7c2a782e74e97676d7
Dam a Tech Guy <104665781+damatechguy27@users.noreply.github.com>
2024-02-15 09:57:45 -0500

    Update S3-bucket_list.ps1

--- S3-bucket_list.ps1
+++ S3-bucket_list.ps1
@@ -1,29 +1,50 @@
+$profiles = 'default'
+
 # Initialize an empty array to store results
 $results = @()
 
-# Get the list of S3 buckets
-$s3Buckets = aws s3api list-buckets --query "Buckets[].Name" --output text
+# Loop through each AWS profile
+foreach ($profile in $profiles) {
+    # Set the AWS profile
+    Set-AWSCredential -ProfileName $profiles
 
-# Loop through each bucket
-foreach ($bucket in $s3Buckets) {
-    # Get the bucket details
-    $bucketDetails = aws s3api get-bucket-location --bucket $bucket | ConvertFrom-Json
+    Set-DefaultAWSRegion -Region 'us-east-1'
 
-    # Get the bucket tags
-    $bucketTags = aws s3api get-bucket-tagging --bucket $bucket | ConvertFrom-Json
+    # Get EC2 instances in the current account
+    $s3 = Get-S3Bucket
+    
 
-    # Extract tag key-value pairs
-    $tags = $bucketTags.TagSet | ForEach-Object {
-        "$($_.Key):$($_.Value)"
-    }
+    # Loop through each EC2 instance
+    foreach ($s3Buckets in $s3) {
+        # Get instance ID
+        $lc_policy_config = Get-S3LifecycleConfiguration -BucketName $s3Buckets.BucketName -ErrorAction SilentlyContinue
+        # Get 'data priority' tag
+        $AppTag = (Get-S3BucketTagging -BucketName $s3Buckets.BucketName | Where-Object { $_.Key -eq 'Application' }).Value
 
-    # Add the result to the array
-    $results += [PSCustomObject]@{
-        'S3 bucket name' = $bucket
-        'Location' = $bucketDetails.LocationConstraint
-        'Tags' = $tags -join ', '
+        # Get encryption status
+        $encryptionStatus = (Get-S3BucketEncryption -BucketName $s3Buckets.BucketName).ServerSideEncryptionConfiguration.Rules.Count -gt 0
+  
+        
+
+        if ($lc_policy_config.Rules.Count -gt 1 ){$lc_policy_status="Enabled"} else {$lc_policy_status="Disabled"}
+        #$lc_policy_status
+        # Get instance status
+        #$instanceStatus = $instanceHealth.InstanceState.Name
+        #$s3Buckets.BucketName       
+
+        # Add the result to the array
+        $results += [PSCustomObject]@{
+            'Profile' = $profile
+            'Bucket Name' = $s3Buckets.BucketName
+            'LifeCyclePolicy Status' = $lc_policy_status
+            'App' = $AppTag
+            'Encryption' = $encryptionStatus
+       }
     }
 }
 
+#$instanceHealth.InstanceState.Name 
+
 # Export the results to a CSV file
-$results | Export-Csv -Path "S3_Buckets_Specs_Tags.csv" -NoTypeInformation
+$results | Export-Csv -Path "S3_Lifecycle_policy_report.csv" -NoTypeInformation
+    
